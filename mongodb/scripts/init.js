1269956181d12dad3e3ad6f668b02601af75987f
sorrmybad3 <smbad3@outlook.com>
2024-08-23 17:45:53 -0500

    Run tests in an isolated container.

--- mongodb/scripts/init.js
+++ mongodb/scripts/init.js
@@ -6,7 +6,8 @@ catch {
   rs.initiate({
     _id: 'rs0',
     members: [
-      { _id: 0, host: 'localhost:27017' },
+      // { _id: 0, host: 'localhost:27017' },
+      { _id: 0, host: 'e2e_mongo:27017' },
     ]
   })
   print('Successfully initiated');
