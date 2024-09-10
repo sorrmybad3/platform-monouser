aa4de161e84d5990a265a2bb24cbcf199d035c15
sorrmybad3 <smbad3@outlook.com>
2024-09-10 10:42:29 -0500

    Adding cors to server.

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -14,11 +13,12 @@ async function bootstrap() {
     .addTag('SUIT')
     .addBearerAuth()
     .build();
+
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-  app.useGlobalPipes(new ValidationPipe());
   app.enableCors();
+
   await app.listen(3000);
 }
 bootstrap();
